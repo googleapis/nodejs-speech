d71458a3c527852a8cf2ed5870e14dfbdd29f9bd
Kristin Galvin <galvink@google.com>
2019-04-03 16:39:51 -0700

    new sample for infinite streaming command line app

---
+++ samples/infinite-streaming-cli/eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "extends": ["eslint:recommended", "google"],
+  "rules": {
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    // allow console and debugger in development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  }
+}
