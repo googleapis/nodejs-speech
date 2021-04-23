45f41cf1b8c5c3fde564224aaa1a85f62cb5a987
bcoe <bencoe@google.com>
2021-04-22 17:49:51 -0700

    build(owlbot): move speech to owlbot

--- owlbot.py
+++ owlbot.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,31 +14,32 @@
 
 """This script is used to synthesize generated parts of this library."""
 
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This script is used to synthesize generated parts of this library."""
+
+import os
 import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.node as node
+import json
 import logging
+from pathlib import Path
 
-logging.basicConfig(level=logging.DEBUG)
-
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
-
-gapic = gcp.GAPICBazel()
-common_templates = gcp.CommonTemplates()
-
-versions = ['v1p1beta1', 'v1']
+node.owlbot_main(templates_excludes=["src/index.ts"])
 name = 'speech'
-
-for version in versions:
-    library = gapic.node_library(name, version)
-
-    # skip index, protos, package.json, and README.md
-    s.copy(
-        library,
-        excludes=['package.json', 'src/index.ts',]
-    )
-
+for version in node.detect_versions():
     # Manual helper methods override the streaming API so that it
     # accepts streamingConfig when calling streamingRecognize.
     # Rename the generated methods to avoid confusion.
@@ -50,8 +51,4 @@ for version in versions:
         '// eslint-disable-next-line @typescript-eslint/no-empty-interface\n' +
         'export interface SpeechClient extends ImprovedStreamingClient {}\n'
     )
-
-templates = common_templates.node_library(source_location='build/src')
-s.copy(templates)
-
-node.postprocess_gapic_library()
+node.postprocess_gapic_library_hermetic()
