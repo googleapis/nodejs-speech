fbf9aad599c53fbca34f7e0db65558a2a71e3500
b-loved-dreamer <franklinsnunez@gmail.com>
2021-06-15 13:22:49 -0700

    chore: solves resource id issues

--- samples/system-test/modelAdaptation.test.js
+++ samples/system-test/modelAdaptation.test.js
@@ -29,8 +29,8 @@ const adaptationClient = new speech.AdaptationClient();
 
 const projectId = process.env.GCLOUD_PROJECT;
 const location = 'global'
-const customClassId = uuidv4().replace(/-/g, '').substring(0, 15);
-const phraseSetId = uuidv4().replace(/-/g, '').substring(0, 15);
+const customClassId = uuidv4().replace(/-/g, '').substring(0, 8);
+const phraseSetId = uuidv4().replace(/-/g, '').substring(0, 8);
 const classParent = `projects/${projectId}/locations/${location}/customClasses/${customClassId}`;
 const phraseParent = `projects/${projectId}/locations/${location}/phraseSets/${customClassId}`;
 
