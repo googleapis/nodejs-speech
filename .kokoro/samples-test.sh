ce770ff5b0d3359538686579274ec3d8a86d6d60
Sofia Leon <sofialeon@google.com>
2022-12-06 14:43:39 -0800

    test new secret retrieval

--- .kokoro/samples-test.sh
+++ .kokoro/samples-test.sh
@@ -19,10 +19,9 @@ set -eo pipefail
 export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
 # Setup service account credentials.
-export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secret_manager/long-door-651-kokoro-system-test-service-account
 export GCLOUD_PROJECT=long-door-651
 
-export NOT_REAL_SECRET=${SECRET_LOCATION}/not_a_real_secret_testing_permissions
 
 cd $(dirname $0)/..
 
